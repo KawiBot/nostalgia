bda1dbd4bc0a6563617a359b7c784322f80516f9
Kawika Mendiola <kwkmendiolabusiness@gmail.com>
2022-05-21 04:15:31 -0700

    Added Posts, Form, redux framework.

--- server/index.js
+++ server/index.js
@@ -2,5 +2,24 @@ import express from "express"
 import bodyParser from "body-parser"
 import mongoose from "mongoose"
 import cors from "cors"
+import postRoutes from "./routes/posts.js"
 
+const app = express()
 
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+app.use(cors())
+
+app.use("/posts", postRoutes)
+const CONNECTION_URL =
+  "mongodb+srv://Nostalgia:1234@cluster0.y5of8.mongodb.net/?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 8080
+
+mongoose
+  .connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
+  .catch((error) => console.error(error))
